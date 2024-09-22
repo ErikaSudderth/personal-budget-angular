8ae03a7f3da3f60f2ceeabd8b60514bf66eace42
ErikaSudderth <erikasudderth@gmail.com>
2024-09-22 11:02:44 -0400

    Added Angular

---
+++ personal-budget/src/app/homepage/homepage.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Chart } from 'chart.js';
+
+interface DataSource {
+  datasets: {
+    data: number[];
+    backgroundColor: string[];
+  }[];
+  labels: string[];
+}
+
+@Component({
+  selector: 'pb-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrl: './homepage.component.scss',
+})
+export class HomepageComponent implements OnInit {
+  public dataSource: DataSource = {
+    datasets: [
+      {
+        data: [],
+        backgroundColor: [
+          '#ffcd56',
+          '#ff6384',
+          '#36a2eb',
+          '#fd6b19',
+          '#00FF00',
+          '#800080',
+          '#FFA500',
+        ],
+      },
+    ],
+    labels: [],
+  };
+
+  constructor(private http: HttpClient, private renderer: Renderer2) {}
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:3000/budget').subscribe((res: any) => {
+      console.log(res.myBudget);
+      for (var i = 0; i < res.myBudget.length; i++) {
+        this.dataSource.datasets[0].data[i] = res.myBudget[i].budget;
+        this.dataSource.labels[i] = res.myBudget[i].title;
+      }
+      this.createChart();
+    });
+  }
+
+  createChart() {
+    // var ctx = this.renderer
+    //   .selectRootElement('#myChart', true)
+    //   .getContext('2d');
+    var ctx = this.renderer.selectRootElement('#myChart', true);
+    var myPieChart = new Chart(ctx, {
+      type: 'pie',
+      data: this.dataSource,
+    });
+  }
+}
